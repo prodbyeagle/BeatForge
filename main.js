144222d71e9f646185ab7fe5bf648633500f04b6
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-04-18 02:30:53 +0200

    Deleted Code

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ app.on('ready', () => {
         height: 900,
         maxWidth: 1920,
         maxHeight: 1080,
-        minWidth: 400,
-        minHeight: 500,
+        minWidth: 800,
+        minHeight: 900,
+        autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true // Zugriff auf Node.js-Module im Renderer-Prozess aktivieren
         }
@@ -19,26 +20,6 @@ app.on('ready', () => {
 
     // HTML-Datei im Hauptfenster laden
     mainWindow.loadFile(path.join(__dirname, 'sites', 'onboarding.html'));
-
-    // IPC-Ereignislistener für Anforderungen des Renderer-Prozesses hinzufügen
-    ipcMain.on('check-onboarding', (event) => {
-        // Überprüfen, ob die Benutzerdaten vorhanden sind
-        const userDataPath = path.join(app.getPath('userData'), 'userData.json');
-        if (fs.existsSync(userDataPath)) {
-            // Benutzerdaten vorhanden, JSON-Datei lesen
-            const userData = JSON.parse(fs.readFileSync(userDataPath, 'utf8'));
-            if (userData.onboarding_complete) {
-                // Onboarding abgeschlossen, an Renderer-Prozess senden
-                event.reply('onboarding-status', true);
-            } else {
-                // Onboarding nicht abgeschlossen, an Renderer-Prozess senden
-                event.reply('onboarding-status', false);
-            }
-        } else {
-            // Benutzerdaten nicht vorhanden, an Renderer-Prozess senden
-            event.reply('onboarding-status', false);
-        }
-    });
 });
 
 app.on('window-all-closed', () => {
