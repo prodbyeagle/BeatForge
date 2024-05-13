de9427cda77b8886ecbe7b38c7c69830f71383c9
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-05-13 16:15:39 +0200

    Fixed Wrong Version, made Update Logs better

--- js/home.js
+++ js/home.js
@@ -294,3 +294,30 @@ document.addEventListener("keydown", function (event) {
         document.documentElement.style.filter =
           "grayscale(60%) brightness(60%)";
       });
+
+      //* Scroll Code
+
+var modal = document.getElementById('modal');
+var isScrolling = false;
+var startY;
+var startScrollTop;
+
+modal.addEventListener('mousedown', function(e) {
+    isScrolling = true;
+    startY = e.pageY;
+    startScrollTop = modal.scrollTop;
+});
+
+modal.addEventListener('mousemove', function(e) {
+    if (!isScrolling) return;
+    var delta = startY - e.pageY;
+    modal.scrollTop = startScrollTop + delta;
+});
+
+modal.addEventListener('mouseup', function() {
+    isScrolling = false;
+});
+
+modal.addEventListener('mouseleave', function() {
+    isScrolling = false;
+});
