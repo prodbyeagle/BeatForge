76d554a0221e5b46f1ebdfff208d56c3cc3c804f
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-05-13 16:16:01 +0200

    Added Tray and made an non 2nd Window Check

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //main.js
 
-const { app, dialog, ipcMain, BrowserWindow } = require("electron");
+const { app, Tray, Menu, dialog, ipcMain, BrowserWindow } = require("electron");
 const path = require("path");
 const fs = require("fs");
 
 const configFilePath = path.join(app.getPath("userData"), "config.json");
 const themesFilePath = path.join(__dirname, "themes.json");
-const langDir = path.join(__dirname, "language");
+
 let mainWindow;
 let windowMaximized = false;
 
@@ -77,83 +77,135 @@ function createConfigFile() {
   }
 }
 
-app.on("ready", () => {
-  createConfigFile();
+const gotTheLock = app.requestSingleInstanceLock();
 
-  ipcMain.on("onboarding-complete", (_, userData) => {
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on("second-instance", () => {
+    dialog.showMessageBox({
+      type: "info",
+      title: "Hey!",
+      message: "The App is already open.",
+    });
+  });
+
+  app.on("ready", () => {
+//* Tray
+  tray = new Tray(path.join(__dirname, 'assets', 'testicon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Beatforge v0.2.0',
+      enabled: false
+    },
+    { type: 'separator' },
+    {
+      label: 'Open Beatforge',
+      click: () => {
+        if (mainWindow === null) {
+          createMainWindow();
+        } else {
+          mainWindow.show();
+        }
+      }
+    },
+    {
+      label: 'Close Beatforge',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]);
+  tray.setContextMenu(contextMenu);
+
+    createConfigFile();
+
+    ipcMain.on("onboarding-complete", (_, userData) => {
+      const config = loadConfig();
+
+      config.onboardingCompleted = true;
+
+      saveConfig(config);
+
+      if (userData.relaunchApp) {
+        console.log("App wird neu gestartet...");
+        app.relaunch();
+        app.quit();
+      }
+    });
+
     const config = loadConfig();
+    if (config.onboardingCompleted) {
+      mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 1000,
+        maxWidth: 1920,
+        maxHeight: 1080,
+        minWidth: 800,
+        minHeight: 1000,
+        autoHideMenuBar: true,
+        fullscreenable: false,
+        frame: false,
+        icon: path.join(__dirname, "assets", "testicon.png"),
+        webPreferences: {
+          nodeIntegration: true,
+          preload: path.join(__dirname, "preload.js"),
+        },
+      });
 
-    config.onboardingCompleted = true;
+      mainWindow.once("ready-to-show", () => {
+        sendThemesToRenderer();
+      });
 
-    saveConfig(config);
+      mainWindow.loadFile(path.join(__dirname, "sites", "home.html"));
 
-    if (userData.relaunchApp) {
-      console.log("App wird neu gestartet...");
-      app.relaunch();
+      mainWindow.on("close", () => {
+        mainWindow = null;
+      });
+    } else {
+      let mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 1000,
+        maxWidth: 1920,
+        maxHeight: 1080,
+        minWidth: 800,
+        minHeight: 1000,
+        autoHideMenuBar: true,
+        fullscreenable: false,
+        frame: false,
+        icon: path.join(__dirname, "assets", "testicon.png"),
+        webPreferences: {
+          nodeIntegration: true,
+          preload: path.join(__dirname, "preload.js"),
+        },
+      });
+
+      mainWindow.loadFile("index.html");
+
+      mainWindow.once("ready-to-show", () => {
+        sendThemesToRenderer();
+      });
+
+      mainWindow.loadFile(path.join(__dirname, "sites", "intro.html"));
+
+      mainWindow.on("close", () => {
+        app.quit();
+      });
+    }
+  });
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
       app.quit();
     }
   });
 
-  const config = loadConfig();
-  if (config.onboardingCompleted) {
-    mainWindow = new BrowserWindow({
-      width: 1280,
-      height: 1000,
-      maxWidth: 1920,
-      maxHeight: 1080,
-      minWidth: 800,
-      minHeight: 1000,
-      autoHideMenuBar: true,
-      fullscreenable: false,
-      frame: false,
-      icon: path.join(__dirname, "assets", "testicon.png"),
-      webPreferences: {
-        nodeIntegration: true,
-        preload: path.join(__dirname, "preload.js"),
-      },
-    });
-
-    mainWindow.once("ready-to-show", () => {
-      sendThemesToRenderer();
-    });
-
-    mainWindow.loadFile(path.join(__dirname, "sites", "home.html"));
-
-    mainWindow.on("close", () => {
-      mainWindow = null;
-    });
-  } else {
-    let mainWindow = new BrowserWindow({
-      width: 1280,
-      height: 1000,
-      maxWidth: 1920,
-      maxHeight: 1080,
-      minWidth: 800,
-      minHeight: 1000,
-      autoHideMenuBar: true,
-      fullscreenable: false,
-      frame: false,
-      icon: path.join(__dirname, "assets", "testicon.png"),
-      webPreferences: {
-        nodeIntegration: true,
-        preload: path.join(__dirname, "preload.js"),
-      },
-    });
-
-    const defaultLangData = loadLanguageFile("en");
-    mainWindow.loadFile("index.html");
-
-    mainWindow.once("ready-to-show", () => {
-      sendThemesToRenderer();
-    });
-
-    mainWindow.loadFile(path.join(__dirname, "sites", "intro.html"));
-
-    mainWindow.on("close", () => {
-      app.quit();
-    });
-  }
-});
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+}
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
